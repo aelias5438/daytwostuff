9fff6bfab579dca1f0a1dba1f654c9b17673daae
Alex Elias <65088695+aelias5438@users.noreply.github.com>
2026-01-15 19:59:14 +0000

    First commit

---
+++ day2.py
@@ -0,0 +1,21 @@
+#%%
+# /workspaces/daytwostuff (main)
+#%%
+# python3 -m venv .venv
+# It should be in the repo to ensure it follows requirements.txt
+#%%
+# /workspaces/daytwostuff (main)
+# It is the same
+
+#%%
+import pandas as pd
+data = pd.read_csv('data.csv')
+data.head()
+
+#%%
+# Extensions can be installed locally or in the codespace, and extensions. Different sections for local extensions, codespaces extensions, and reccommended extensions
+# clean, spreadsheet style view, shows missing values, great for data cleaning
+
+
+#%%
+# Plotly 6.5.1
